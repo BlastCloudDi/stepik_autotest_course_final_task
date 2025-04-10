1add30604fb629ed14a30248a3eb18d79f9819b7
BlastCloudDi <anaidcom958@gmail.com>
2025-04-11 02:14:58 +0500

    added test groups and setup from lesson 4.3 step 13

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
@@ -60,6 +61,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
@@ -68,3 +70,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty()
     basket_page.should_be_empty_message()
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login_page.should_be_login_page()
+        email = str(time.time()) + "@fakemail.org"
+        password = "SuPerUn1kP@ss"
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+        yield
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
+    
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.should_be_product_added_to_basket()
